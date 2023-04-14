7aa74a63f4d5bafb63df17c65a3a754de1b67f46
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-04-14 14:56:12 -0400

    🐛 FIX: String did not have character size

--- backend/database/models.py
+++ backend/database/models.py
@@ -25,23 +25,23 @@ class Car(db.Model):
     model = db.Column(db.String(255), nullable=False)
     year = db.Column(db.Integer)
     # Adds user_id as an Integer column on the car table which references the id column on user table
-    user_username=db.Column(db.String, db.ForeignKey('user.username'))
+    user_username=db.Column(db.String(255), db.ForeignKey('user.username'))
     # Establishes object relation between car-user so we can grab values like car.user.username
     user = db.relationship("User")
 
 # TODO: Add your models below, remember to add a new migration and upgrade database
 class Reviews(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    book_id=db.Column(db.String, nullable=False)
-    review_text=db.Column(db.String, nullable=False)
+    book_id=db.Column(db.String(255), nullable=False)
+    review_text=db.Column(db.String(255), nullable=False)
     rating=db.Column(db.Integer, nullable=False)
-    user_username=db.Column(db.String, db.ForeignKey('user.username'))
+    user_username=db.Column(db.String(255), db.ForeignKey('user.username'))
     user = db.relationship("User")
 
 class FavoriteBooks(db.Model):
     id =db.Column(db.Integer, primary_key=True)
-    book_id=db.Column(db.String, nullable=False)
-    title=db.Column(db.String, nullable=False)
-    thumbnail_url=db.Column(db.String, nullable=False)
-    user_username=db.Column(db.String, db.ForeignKey('user.username'))
+    book_id=db.Column(db.String(255), nullable=False)
+    title=db.Column(db.String(255), nullable=False)
+    thumbnail_url=db.Column(db.String(255), nullable=False)
+    user_username=db.Column(db.String(255), db.ForeignKey('user.username'))
     user = db.relationship("User")
