5e045c365cac155d980112d799f75ae0ac6d633a
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-04-19 10:59:47 -0400

    📦 NEW: results now display cards when searching

--- frontend/src/components/Search/ResultCards.jsx
+++ frontend/src/components/Search/ResultCards.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import './ResultCards.css'
 
 const ResultCards = ({bookResults}) => {
     return (
-        <div>
+        <div className='results'>
+            {bookResults.map((book) => {
+                return(
+                    <div className='card'>
+                        <img src={book.volumeInfo.imageLinks.thumbnail} alt={`${book.volumeInfo.title} cover`} className='thumbnail'/>
+                        <div className='info'>
+                            <h2>{book.volumeInfo.title}</h2>
+                            <p>{book.volumeInfo.description}</p>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
     );
 }
