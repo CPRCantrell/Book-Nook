47d935527a6738d77887ce78f8bd6795f4f86d22
Blase Moylan <moylanblase@gmail.com>
2023-04-21 09:30:28 -0700

    📦 NEW: working on recommended

--- frontend/src/components/Home/Recommended.jsx
+++ frontend/src/components/Home/Recommended.jsx
@@ -1,23 +1,56 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+
 const Recommended = () => {
-    const [books,setBooks]=useState([])
+    const [bookResults,setBookResults]=useState([])
 
     async function getBooks(){
         try{
             let results= await axios.get('http://127.0.0.1:5000/api/book/booksWithReviews')
             console.log(results)
+            // setBookResults(results.data)
+
         }catch(ex){
             console.log("issue with getBooks")
         }
     }
 
+    // function checkForImg(data){
+    //     if(data.imageLinks){
+    //         return(data.imageLinks.thumbnail)
+    //     }
+    //     return(defaultCover)
+    // }
+
     function cards(){
         getBooks()
-        return(
-            <p>This is working!</p>
-        )
+        
+                
+        
+        // return (
+        //     <div className='results'>
+        //         {bookResults.map((book, index) => {
+        //             return(
+        //                 <Link key={index} to={`/detail/${book.id}`}>
+        //                     <div className='card'>
+        //                         <img src={checkForImg(book.volumeInfo)} alt={`${book.volumeInfo.title} cover`} className='thumbnail'/>
+        //                         <div className='rating'>
+        //                             <p>{book.volumeInfo.averageRating ? book.volumeInfo.averageRating:'No rating'} <span>&#9733;</span></p>
+        //                         </div>
+        //                         <div className='info'>
+        //                             <h2>{book.volumeInfo.title}</h2>
+        //                             <p dangerouslySetInnerHTML={{ __html: book.volumeInfo.description }} />
+        //                         </div>
+        //                     </div>
+        //                 </Link>
+        //             )
+        //         })}
+        //     </div>
+        // );
     }
+            
+        
+    
 
     return ( 
         <div>
