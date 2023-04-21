482c135266af4ab4427bbaa7c97308514ec5e463
Blase Moylan <moylanblase@gmail.com>
2023-04-21 08:19:14 -0700

    📦 NEW: built componets for HomePage

---
+++ frontend/src/components/Home/Recommended.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+const Recommended = () => {
+
+    async function getBooks(){
+        
+    }
+
+    return ( 
+        <div></div>
+    );
+}
+ 
+export default Recommended;
